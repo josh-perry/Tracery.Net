115ec575b61f919a014648edc5827e4939e6e93a
Josh Perry <joshperry@outlook.com>
2016-11-17 13:35:10 +0000

    :white_check_mark: Added a test

---
+++ Tests/FlattenTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Tests
+{
+    [TestClass]
+    public class FlattenTests
+    {
+        [TestMethod]
+        public void Flatten_HelloWorld_Success()
+        {
+            // Arrange
+            var json = "{" +
+                       "    'origin': 'hello world'" +
+                       "}";
+
+            var grammar = new TraceryNet.Grammar(json);
+            
+            // Act
+            var output = grammar.Flatten("#origin#");
+
+            // Assert
+            Assert.AreEqual(output, "hello world");
+        }
+    }
+}
