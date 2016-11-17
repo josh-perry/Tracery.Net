a08fc7dca7c19168b574550e0495253fcd86e127
Josh Perry <joshperry@outlook.com>
2016-11-17 00:39:31 +0000

    :sparkles: Added (a few) modifiers

--- Tracery.Net/Grammar.cs
+++ Tracery.Net/Grammar.cs
@@ -4,6 +4,8 @@ using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace TraceryNet
 {
@@ -34,13 +36,24 @@ namespace TraceryNet
         public string Resolve(string rule)
         {
             // Get all expansion symbols
-            var regex = new Regex(@"#\w+#");
+            var regex = new Regex(@"#.+?#");
 
             // Iterate expansion symbols
             foreach (Match match in regex.Matches(rule))
             {
                 // Remove the # surrounding the symbol name
                 var matchName = match.Value.Replace("#", "");
+
+                // Get modifiers
+                var modifiers = new List<string>();
+
+                if (match.Value.Contains("."))
+                {
+                    matchName = matchName.Substring(0, matchName.IndexOf("."));
+
+                    modifiers = match.Value.Replace("#", "").Split('.').ToList();
+                    modifiers.RemoveAt(0);
+                }
 
                 // Get the selected rule
                 var selectedRule = Rules[matchName];
@@ -50,19 +63,58 @@ namespace TraceryNet
                     // If the rule has any children then pick one at random
                     var index = Random.Next(0, ((JArray)selectedRule).Count);
                     var chosen = selectedRule[index].ToString();
-                    
-                    rule = rule.Replace(match.Value, Resolve(chosen));
+                    var resolved = Resolve(chosen);
+
+                    resolved = ApplyModifiers(resolved, modifiers);
+
+                    rule = rule.Replace(match.Value, resolved);
                 }
                 else 
                 {
                     // Otherwise resolve it
                     var resolved = Resolve(selectedRule.ToString());
 
+                    resolved = ApplyModifiers(resolved, modifiers);
+
                     rule = rule.Replace(match.Value, resolved);
                 }
             }
 
             return rule;
+        }
+
+        private string ApplyModifiers(string resolved, List<string> modifiers)
+        {
+            var sentencePunctuation = new List<char> { ',', '.', '!', '?' };
+            char lastChar;
+
+            foreach (var modifier in modifiers)
+            {
+                switch(modifier)
+                {
+                    case "capitalize":
+                        resolved = char.ToUpper(resolved[0]) + resolved.Substring(1);
+                        break;
+                    case "comma":
+                        lastChar = resolved[resolved.Length - 1];
+                        
+                        if(sentencePunctuation.Contains(lastChar))
+                            break;
+
+                        resolved += ",";
+                        break;
+                    case "inQuotes":
+                        resolved = "\"" + resolved + "\"";
+                        break;
+                    case "beeSpeak":
+                        resolved = resolved.Replace("s", "zzz");
+                        break;
+                    default:
+                        continue;
+                }
+            }
+
+            return resolved;
         }
     }
 }
